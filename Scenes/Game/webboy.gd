fbb03875c647d729295a85548c7c1e365e9a9e17
Tralexium <alextheloler@gmail.com>
2024-04-01 17:00:45 +0100

    Reset the position when outside of the game window

--- Scenes/Game/webboy.gd
+++ Scenes/Game/webboy.gd
@@ -7,6 +7,7 @@ extends Node3D
 # Target rotation values
 var target_rotation_y: float = 0.0
 var target_rotation_z: float = 0.0
+var mouse_is_inside: bool = false
 
 func _process(delta):
 	var viewport_size = get_viewport().size
@@ -19,9 +20,16 @@ func _process(delta):
 	# Determine the target rotation based on the cursor position
 	# Adjust these formulas as needed to match the desired effect
 	var _click_mult = click_push_multiplier if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) else 1.0
-	target_rotation_y = -cursor_offset.x * rotation_amnt * _click_mult
-	target_rotation_z = cursor_offset.y * rotation_amnt * _click_mult
+	target_rotation_y = -cursor_offset.x * rotation_amnt * _click_mult * float(mouse_is_inside)
+	target_rotation_z = cursor_offset.y * rotation_amnt * _click_mult * float(mouse_is_inside)
 	
 	# Smoothly interpolate (lerp) current rotation towards the target rotation
 	rotation_degrees.y = lerp(rotation_degrees.y, target_rotation_y, rotation_speed)
 	rotation_degrees.z = lerp(rotation_degrees.z, target_rotation_z, rotation_speed)
+
+func _notification(event):
+	match event:
+		NOTIFICATION_WM_MOUSE_EXIT:
+			mouse_is_inside = false
+		NOTIFICATION_WM_MOUSE_ENTER:
+			mouse_is_inside = true
